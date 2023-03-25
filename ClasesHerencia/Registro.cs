72b2e528ddab66bb4f0c66f19e6bd56f4a1b9907
JairS7u <condochucajair@gmail.com>
2023-03-24 23:30:13 -0400

    Creamos la clase registro

---
+++ ClasesHerencia/Registro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesHerencia
+{
+    internal class Registro : Usuario
+    {
+        public Registro() { }   
+        public string nombre { get; set; }  
+        public string detalles { get; set; }
+    }
+}
